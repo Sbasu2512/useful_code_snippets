71b3faa9cd1566ed22e4eca75bd2c3f6d5ce92ad
Shaan <sayantanbasu@live.in>
2023-05-17 08:48:43 +0530

    practice of different problems

---
+++ Dot Net/session4/session4/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace session4;
+class Employee
+{
+    public string? EmpName;
+    //public string? Dept;
+    public double salary;
+
+    public double GetSalary()
+    {
+        return salary;
+    }
+
+    public static double GetHighestSalary(Employee[] arr)
+    {
+        double highestSal = arr[0].salary;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (highestSal < arr[i].salary)
+            {
+                highestSal = arr[i].salary;
+            }
+            //Console.WriteLine(highestSal);
+        }
+        return highestSal;
+    }
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //Employee x = new Employee();
+        //x.EmpName = "Sayantan";
+        //x.Dept = "IT";
+        //x.salary = 40000;
+
+        Employee[] arr = new Employee[5];
+
+        arr[0] = new Employee();
+        arr[1] = new Employee();
+        arr[2] = new Employee();
+        arr[3] = new Employee();
+        arr[4] = new Employee();
+
+        arr[0].EmpName = "Sayantan";
+        //arr[0].Dept = "IT";
+        arr[0].salary = 32000;
+        //arr[0].EmpName = "Sayantan";
+        arr[1].EmpName = "Abhishek";
+        arr[1].salary = 30000;
+        arr[2].EmpName = "Shaurya";
+        arr[2].salary = 35000;
+        arr[3].EmpName = "Falgun";
+        arr[3].salary = 36000;
+        arr[4].EmpName = "Dilip";
+        arr[4].salary = 38000;
+
+
+
+        Console.WriteLine(GetHighestSalary(arr));
+
+        Console.ReadKey();
+    }
+}
+
---
+++ Dot Net/session5/session5/Program.cs
@@ -0,0 +1,114 @@
+﻿namespace session5;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        School school = new School();
+        for(int i=1; i< school.GetLength(); i++)
+        {
+            Console.WriteLine(school[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Course { get; set; }
+
+    public string GetResult()
+    {
+        return $"Id: {this.Id}, Name: {this.Name}, Course: {this.Course}";
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {this.Id}, Name: {this.Name}, Course: {this.Course}";
+    }
+
+
+}
+
+
+class School
+{
+    List<Student> studentList;
+
+    public School()
+    {
+        this.studentList = new List<Student>
+            {
+                new Student { Id = 1, Name = "Alice", Course = "Physics" },
+                new Student { Id = 2, Name = "Bob", Course = "Mathematics" },
+                new Student { Id = 3, Name = "Charlie", Course = "Computer Science" },
+                new Student { Id = 4, Name = "David", Course = "Biology" },
+                new Student { Id = 5, Name = "Eve", Course = "Computer Science" },
+                new Student { Id = 6, Name = "Frank", Course = "English" },
+                new Student { Id = 7, Name = "Grace", Course = "Biology" },
+                new Student { Id = 8, Name = "Alice", Course = "Mathematics" },
+                new Student { Id = 9, Name = "Ian", Course = "Physics" },
+                new Student { Id = 10, Name = "Julia", Course = "Physics" }
+            };
+
+    }
+
+    public int GetLength()
+    {
+        return this.studentList.Count;
+    }
+
+    public Student GetStudent(int Id)
+    {
+        return this.studentList.Find(x => x.Id == Id);
+    }
+
+    public Student this[int Id]
+    {
+        get
+        {
+            return this.studentList.Find(x => x.Id == Id);
+        }
+
+        set
+        {
+            Student s = this.studentList.Find(x => x.Id == Id);
+            if (s != null)
+            {
+                s.Id = value.Id;
+                s.Name = value.Name;
+                s.Course = value.Course;
+            }
+        }
+    }
+
+    public Student this[string name]
+    {
+        get
+        {
+            return this.studentList.Find(x => x.Name == name);
+        }
+
+        set
+        {
+            Student s = this.studentList.Find(x => x.Name == name);
+            if (s != null)
+            {
+                s.Id = value.Id;
+                s.Name = value.Name;
+                s.Course = value.Course;
+            }
+        }
+    }
+
+    public Student this[string name, string course]
+    {
+        get
+        {
+            return this.studentList.Find(x => x.Name == name && x.Course == course);
+        }
+    }
+}
