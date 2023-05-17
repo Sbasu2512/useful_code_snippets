37e685ebc45ddcfdc104bbad98148a035b164375
Shaan <sayantanbasu@live.in>
2023-05-17 08:49:25 +0530

    more code

---
+++ Dot Net/Session2_C/Session2_C/Homework1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class HelloWorld
+{
+    private int randomizer(int min, int max)
+    {
+        Random x = new Random();
+        return x.Next(min, max);
+    }
+     static void Main(string[] args, HelloWorld helloWorld)
+    {
+        string[] names = { "Sudipto", "Sayantan", "Avijit", "Piyali", "Moumita" };
+        //select a random winner -- index
+        int randomIndex = helloWorld.randomizer(0, names.Length);
+        System.Console.WriteLine(names[randomIndex]);
+
+        //select 3 distinct winners
+        string[] winners = new string[3];
+        string randomWinner1 = names[helloWorld.randomizer(0, names.Length)];
+        winners[0] = randomWinner1;
+        string randomWinner2 = names[helloWorld.randomizer(0, names.Length - 1)];
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (!names[i].Contains(randomWinner2))
+            {
+                names[1] = randomWinner2;
+                break;
+            }
+            else
+            {
+                randomWinner2 = names[helloWorld.randomizer(0, names.Length - 1)];
+            }
+        }
+        string randomWinner3 = names[helloWorld.randomizer(0, names.Length - 2)];
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (!names[i].Contains(randomWinner3))
+            {
+                names[2] = randomWinner3;
+                break;
+            }
+            else
+            {
+                randomWinner3 = names[helloWorld.randomizer(0, names.Length - 2)];
+            }
+        }
+    }
+}
---
+++ Dot Net/Session2_C/Session2_C/Homework2.cs
@@ -0,0 +1,43 @@
+﻿namespace Session2_C;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        string[] weekDays = { "Sun", "Mon", "Tues", "Wed", "Thrus", "Fri", "Sat" };
+        Console.Write("When do you want the leave?");
+        string userInput = Console.ReadLine();
+        int startDayIndex, endDayIndex;
+        string startDay, endDay;
+        Console.Write("How many days:");
+        int numberDays = Convert.ToInt32(Console.ReadLine());
+        //Console.ReadKey();
+        if (userInput == null)
+        {
+            throw new ArgumentException("Please enter a day");
+        }
+        for(int i=0; i<weekDays.Length; i++)
+        {
+            if(userInput.ToLower() == weekDays[i].ToLower())
+            {
+                startDayIndex = i;
+                endDayIndex = startDayIndex + numberDays;
+                //Console.WriteLine(endDayIndex);
+                if(endDayIndex != null)
+                {
+                    endDay = weekDays[(endDayIndex % 7)-1];
+                    
+                    if(endDay.ToLower() == "sun" || endDay.ToLower() == "sat")
+                    {
+                        endDay = "Mon";
+                    }
+                    Console.WriteLine($"you will join back on {endDay}");
+                }
+                break;
+            }
+
+        }
+        Console.ReadKey();
+    }
+}
+
